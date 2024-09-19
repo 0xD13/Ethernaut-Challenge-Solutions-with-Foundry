c5bd9f1bac37f59624e21f13d765896693570240
D13 <daniel850113@gmail.com>
2024-09-19 17:49:12 +0800

    Update 18_MagicNumber.s.sol

--- script/18_MagicNumber.s.sol
+++ script/18_MagicNumber.s.sol
@@ -5,11 +5,10 @@ import "forge-std/Script.sol";
 import "../src/Ethernaut Challenge/18_MagicNumber.sol";
 
 contract ExploitScript is Script {
-    MagicNum level18 = MagicNum(payable(0xcE193Bfa1364E0A5A58B4e920D56203C5f62E7Ba));
+    MagicNum level18 = MagicNum(payable(your_challenge_address));
     
     function run() external {
         vm.startBroadcast();
-        console.log(payable(0x03bca201595777C0B67DC5970c4356DaD484A1d6));
         level18.setSolver(address(new MagicNumAttack()));
         vm.stopBroadcast();
     }
